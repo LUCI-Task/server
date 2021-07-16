827968f8b6c5030271f337899b6b67a35867fac3
VodeniZeko <edvin.saletovic@pm.me>
2021-07-15 17:03:55 -0700

    heroku

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import dotenv from "dotenv";
+
+require("dotenv").config();
 
 const server = express();
-dotenv.config();
 
 server.use(express.urlencoded({ limit: "1000mb", extended: true }));
 server.use(express.json({ limit: "50mb" }));
